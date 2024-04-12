d6bb9e0730edd35cd54b9d8256325d2ef04b69c8
robarbosa10 <rogeriocap8@gmail.com>
2024-04-12 19:01:36 -0300

    primeira classe posicao

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import tabuleiro.Posicao;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Posicao p1 = new Posicao(3, 5);
+		
+		System.out.println(p1);
+
+	}
+
+}
